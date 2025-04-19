a703154071d87f259201f8fd8b50b995b6ed4e3c
asyakz <asyakz@gmail.com>
2025-04-19 22:56:04 +0500

    add HW4 sprint 4

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,24 @@
-const initState = {
+const initState: ThemeState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+type ThemeState = {
+    themeId: number
+}
+
+type ThemeAction = {
+    type: 'SET_THEME_ID'
+    id: number
+}
+
+export const themeReducer = (state = initState, action: ThemeAction): ThemeState => { // fix any
     switch (action.type) {
         // дописать
-
+        case 'SET_THEME_ID':
+            return {...state, themeId: action.id}
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): ThemeAction => ({ type: 'SET_THEME_ID', id }) // fix any
